a0761610761e3a3db87f7094a2f5b2a05c2056a8
Ionwyn Sean <isean@sfu.ca>
2019-03-18 05:20:15 -0700

    cest fini

---
+++ README.md
@@ -0,0 +1,31 @@
+# CMPT 361 Assignment 2: Stanford Bunny
+
+## Note
+Please use a browser that supports ES6 JavaScript (i.e. no old IE)
+
+## Features
+- Left-lick and hold to move the bunny around.
+- Scroll your mousewheel to translate the bunny along the Z axis
+- Right-click and hold to rotate the bunny around.
+- Auto-rotating light (stop the rotation by pressing 'p')
+- Auto-panning spot-light (stop the panning by pressing 's')
+- Reset bunny position by pressing 'r'
+- Sloppy implementation of Blinn-Phong shading
+
+## Acknowledgements
+
+Thank you Red Bull, Starbucks Espresso, and these online resources:
+- [Computer Graphics Lecture of Louisiana State University](https://csc.lsu.edu/~kooima/courses/csc4356/)
+- [Geert Arien's Article on Blinn-Phong Shading](http://www.geertarien.com/blog/2017/08/30/blinn-phong-shading-using-webgl/)
+- [Utility function for setting uniform 3 vector](https://kitware.github.io/vtk-js/api/Rendering_OpenGL_ShaderProgram.html)
+
+Perhaps, the most notable acknowledgement goes to Geert Arien.  Despite giving a tutorial on Blinn-Phong lighting on a cube, his methods are applicable elsewhere (e.g. Stanford Bunny)
+
+## Instructions
+
+Run `index.html` on your favourite browser (except for IE).
+
+## Shortcomings
+Despite understanding the principles of Blinn-Phong Lighting, I still struggle to implement it.  There was a lot of trial and error being done for the vertex and fragment shader components.
+I could have mess around with the values that make up the ambient, light, and surface colours.  But, I don't have enough time.
+Also, being under serious time pressure, I decide to leave the project as it is.
--- shader.js
+++ shader.js
@@ -1,135 +1,226 @@
-/**
- *  @author Luke Hartman <lhartman3@zagmail.gonzaga.edu>
- *  @requires WebGL: Common
- *  @description This program demonstrates two different Blinn-Phonglighting effects on a bunny.
- */
+// CMPT 361 Assignment 2: Stanford Bunny
+// Ionwyn Sean || isean || 301267286
 
-"use strict";
-
-let canvas;
-
-/** @type {WebGLRenderingContext} */
 let gl;
-
-let points = [];
-let normals = [];
-
+let canvas;
 let program;
-
-let rot1;
-let rot2;
-let rot3;
-let scale1;
-let t1;
-let t2;
-
+let points = [];
+let indices = [];
+let bunny = [];
 let angle = 0;
-
-let lx = 0;
-let ly = 0;
+let copy_angle = 0;
+let rotation1;
+let rotation2;
+let scale1;
+let trans1;
+let trans2;
+let p_x = 0;
+let copy_lx = 0;
+let p_y = 0;
+let stop_light_rotating = false;
+let stop_light_panning = false;
 
 let status;
 
-// Utility function to make it easier to set a uniform vec3.
-function setUniform3f(prog, name, x, y, z) {
-    let location = gl.getUniformLocation(prog, name);
-    gl.uniform3f(location, x, y, z);
-}
+window.onload = function init() {
+  rotation1 = document.getElementById("rotation1");
+  rotation2 = document.getElementById("rotation2");
+  scale1 = document.getElementById("scale1");
+  trans1 = document.getElementById("trans1");
+  trans2 = document.getElementById("trans2");
+  status = document.getElementById("status");
 
-window.onload = function init()
-{
-    status = document.getElementById("status");
-    rot1 = document.getElementById("rot1");
-    rot2 = document.getElementById("rot2");
-    rot3 = document.getElementById("rot3");
-    scale1 = document.getElementById("scale1");
-    t1 = document.getElementById("t1");
-    t2 = document.getElementById("t2");
-    [rot1, rot2, rot3, scale1, t1, t2].forEach(function(elem) {
-        elem.initValue = elem.value;
-        elem.addEventListener("dblclick", function() {
-            elem.value = elem.initValue;
-        });
-    });
-    canvas = document.getElementById( "gl-canvas" );
-    canvas.addEventListener("mousemove", function(event) {
-        // get mouse position relative to the canvas
-        lx = 2 * event.clientX / canvas.width - 1;
-        ly = 2 * (canvas.height - event.clientY) / canvas.height - 1;
-    });
+  canvas = document.getElementById("gl-canvas");
+  canvas.focus();
+  let drag = false;
+  let dragStart;
+  let dragEnd;
 
-    gl = WebGLUtils.setupWebGL( canvas );
-    if ( !gl ) { alert( "WebGL isn't available" ); }
+  canvas.addEventListener("wheel", ({ deltaY }) => {
+    if (deltaY < 0) {
+      document.getElementById("scale1").value += 0.05;
+    }
+    if (deltaY > 0) {
+      document.getElementById("scale1").value -= 0.05;
+    }
+  });
 
-    gl.viewport( 0, 0, canvas.width, canvas.height );
-    gl.clearColor( 0.8, 0.8, 0.8, 1.0 );
+  canvas.addEventListener("mousedown", ({ which, pageX, pageY }) => {
+    switch (which) {
+      case 1:
+        dragStart = {
+          x: pageX,
+          y: pageY
+        };
 
-    gl.enable(gl.DEPTH_TEST);
+        drag = true;
 
-    // Set points and normals to be what is stored in "bunny.js"
-    points = bunny_pos;
-    normals = bunny_norm;
+        break;
 
-    program = initShaders( gl, "vertex-shader", "fragment-shader" );
-    gl.useProgram( program );
+      case 3:
+        dragStart = {
+          x: pageX,
+          y: pageY
+        };
 
-    let nBuffer = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, nBuffer );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(normals), gl.STATIC_DRAW );
+        drag = true;
 
-    let vNormal = gl.getAttribLocation( program, "vNormal" );
-    gl.vertexAttribPointer( vNormal, 3, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vNormal );
+        break;
+    }
+  });
+  canvas.addEventListener("mousemove", ({ which, clientX, clientY }) => {
+    switch (which) {
+      case 1:
+        // get mouse position with respect to canvas
+        p_x = (2 * clientX) / canvas.width - 1;
+        p_y = (2 * (canvas.height - clientY)) / canvas.height - 1;
+        if (drag) {
+          dragEnd = {
+            x: p_x,
+            y: p_y
+          };
+          document.getElementById("trans1").value = p_x.toFixed(2);
+          document.getElementById("trans2").value = p_y.toFixed(2);
+          dragStart = dragEnd;
+        }
+        break;
+      case 3:
+        p_x = (2 * clientX) / canvas.width - 1;
+        p_y = (2 * (canvas.height - clientY)) / canvas.height - 1;
 
-    let vBuffer = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
+        if (drag) {
+          dragEnd = {
+            x: p_x * 180,
+            y: p_y * 180
+          };
+          document.getElementById("rotation1").value = dragEnd.x.toFixed(2);
+          document.getElementById("rotation2").value = dragEnd.y.toFixed(2);
+          dragStart = dragEnd;
+        }
 
-    let vPosition = gl.getAttribLocation( program, "vPosition" );
-    gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vPosition );
+        break;
+    }
+  });
 
-    window.requestAnimationFrame(render);
+  canvas.addEventListener("mouseup", event => {
+    drag = false;
+  });
+
+  window.addEventListener("keypress", ({ which }) => {
+    switch (which) {
+      case 114:
+        document.getElementById("rotation1").value = 0;
+        document.getElementById("rotation2").value = 0;
+        document.getElementById("scale1").value = 0.25;
+        document.getElementById("trans1").value = 0;
+        document.getElementById("trans2").value = 0;
+
+        break;
+
+      case 112:
+        // One clever way to keep switching is to use XOR operator!
+        stop_light_rotating ^= true;
+        copy_angle = angle;
+        break;
+
+      case 115:
+        // One clever way to keep switching is to use XOR operator!
+        stop_light_panning ^= true;
+        copy_lx = p_x;
+
+        break;
+    }
+  });
+
+  gl = WebGLUtils.setupWebGL(canvas);
+  if (!gl) {
+    alert("WebGL isn't available");
+  }
+
+  gl.viewport(0, 0, canvas.width, canvas.height);
+  gl.clearColor(0.478, 0.472, 0.4703, 1.0);
+
+  gl.enable(gl.DEPTH_TEST);
+  gl.enable(gl.CULL_FACE);
+
+  // Set points from "bunny.js"
+  indices = get_faces();
+  points = get_vertices();
+
+  for (var i = 0; i < indices.length; i++) {
+    for (var j = 0; j < 3; j++) {
+      bunny.push(points[indices[i][j] - 1]);
+    }
+  }
+
+  program = initShaders(gl, "vertex-shader", "fragment-shader");
+  gl.useProgram(program);
+
+  const normBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, normBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, flatten(bunny), gl.STATIC_DRAW);
+
+  const vertexNormal = gl.getAttribLocation(program, "vNormal");
+  gl.vertexAttribPointer(vertexNormal, 3, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(vertexNormal);
+
+  const vertexPosition = gl.getAttribLocation(program, "vPosition");
+  gl.vertexAttribPointer(vertexPosition, 3, gl.FLOAT, false, 0, 0);
+  gl.enableVertexAttribArray(vertexPosition);
+
+  window.requestAnimationFrame(render);
 };
 
-function render()
-{
+// Utility function.  Brilliant idea obtained from: https://kitware.github.io/vtk-js/api/Rendering_OpenGL_ShaderProgram.html
+function setUniform3f(prog, name, x, y, z) {
+  const position = gl.getUniformLocation(prog, name);
+  gl.uniform3f(position, x, y, z);
+}
+
+function render() {
+  if (stop_light_rotating) {
+    angle = copy_angle;
+  } else {
     angle += 0.1;
-    status.innerHTML = "Angles: " + (+rot1.value).toFixed()
-        + ", " + (+rot2.value).toFixed()
-        + ", " + (+rot3.value).toFixed()
-        + ". Scale: " + (+scale1.value).toFixed(2)
-        + ". Translation: " + (+t1.value).toFixed(2)
-        + ", " + (+t2.value).toFixed(2);
-    
-    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    let r1 = rotateX(rot1.value);
-    let r2 = rotateY(rot2.value);
-    let r3 = rotateZ(rot3.value);
-    let s1 = scalem(scale1.value, scale1.value, scale1.value);
-    let trans = translate(t1.value, t2.value, 0);
+  }
 
-    let mat = mult(trans, mult(s1, mult(r3, mult(r2, r1))));
+  if (stop_light_panning) {
+    p_x = copy_lx;
+  } else {
+    p_x += 0.25;
+  }
 
-    let location = gl.getUniformLocation(program, "mat");
-    gl.uniformMatrix4fv(location, false, flatten(mat));
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+  const rotate1 = rotateX(rotation1.value);
+  const rotate2 = rotateY(rotation2.value);
+  const s1 = scalem(scale1.value, scale1.value, scale1.value);
+  const trans = translate(trans1.value, trans2.value, 0);
 
-    let rotationSpeedMultiplier = 10;
-    // x = cos(angle in radians)
-    let x = Math.cos(( Math.PI * angle * rotationSpeedMultiplier ) / 180.0);
-    // y = sin(angle in radians)
-    let y = Math.sin(( Math.PI * angle * rotationSpeedMultiplier ) / 180.0);
+  const mat = mult(trans, mult(s1, mult(rotate2, rotate1)));
 
-    // Set uniforms as appropriate
-    setUniform3f(program, "lightDir", x, y, 0.0);
-    setUniform3f(program, "lightDirM", lx, ly, 3.0);    
-    setUniform3f(program, "lightColor", 0.831, 0.686, 0.216);
-    setUniform3f(program, "lightColorM", 0.753, 0.753, 0.753);
-    setUniform3f(program, "ambientColor", 0.1, 0.1, 0.3); 
-    setUniform3f(program, "surfaceDiffuse", 0.3, 0.2, 0.3);
-    setUniform3f(program, "surfaceSpec", 0.8, 0.8, 0.8);
-    setUniform3f(program, "surfaceSpecM", 0.8, 0.8, 0.8);
+  const position = gl.getUniformLocation(program, "mat");
+  gl.uniformMatrix4fv(position, false, flatten(mat));
 
-    gl.drawArrays( gl.TRIANGLES, 0, points.length );
-    window.requestAnimationFrame(render);
+  const rotationSpeedConstant = 10;
+
+  // x = cos(angle in rad)
+  const x = Math.cos((Math.PI * angle * rotationSpeedConstant) / 180.0);
+  // y = sin(angle in rad)
+  const y = Math.sin((Math.PI * angle * rotationSpeedConstant) / 180.0);
+
+  const gx = Math.cos((Math.PI * p_x * rotationSpeedConstant) / 180.0);
+  // Set uniforms
+  setUniform3f(program, "panning_spotlightDir", gx, p_y, 3.0);
+  setUniform3f(program, "lightDir", x, y, 0.0);
+  setUniform3f(program, "lightColorMat", 1, 1, 0);
+  setUniform3f(program, "lightColor", 0.913, 0.826, 0.216);
+  setUniform3f(program, "ambientColor", 0.113, 0.126, 0.216);
+  setUniform3f(program, "surfaceSpec", 0.2, 0.2, 0.2);
+  setUniform3f(program, "surfaceSpecMat", 0.8, 0.8, 0.8);
+  setUniform3f(program, "surfaceDiffuse", 0.4, 0.3, 0.4);
+
+  gl.bufferData(gl.ARRAY_BUFFER, flatten(bunny), gl.STATIC_DRAW);
+
+  gl.drawArrays(gl.TRIANGLES, 0, bunny.length);
+  window.requestAnimationFrame(render);
 }
